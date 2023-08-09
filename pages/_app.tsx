722deb39f7c377cbc0ea179e71915be6e2ffe460
Jordy <jordybeer@duck.com>
2023-08-09 19:45:49 +0200

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from './components/Layout'
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+        <Layout>
+      <App />
+      </Layout>
+    </ChakraProvider>
+  )
+}
