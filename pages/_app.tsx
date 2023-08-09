a4b0fb4ee3191f1a6405d86faaf5d2dc401105d2
Jordy <jordybeer@duck.com>
2023-08-09 19:54:11 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from './components/Layout'
+import Layout from '../components/layout'
 function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
